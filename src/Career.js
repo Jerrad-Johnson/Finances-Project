91825e7489cbb022cc907c23ed1e8655d4c6e2b4
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-04-09 23:12:31 -0600

    Adding to the project's structure.

---
+++ src/Career.js
@@ -0,0 +1,33 @@
+let jobsData = [];
+
+function JobContainer() {
+
+    function calculateIncome(){
+
+    }
+
+    function calculateSteps(){
+        
+    }
+
+    return (
+      <>
+          <span></span>
+      </>
+    );
+
+}
+
+function Career() {
+    return (
+      <>
+          <span>Test</span>
+          <br />
+          <JobContainer
+               jobsData = {jobsData}
+          />
+      </>
+    );
+}
+
+export default Career;
