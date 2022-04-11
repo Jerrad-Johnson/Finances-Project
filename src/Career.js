19601af8c37c276455f0c0132efc2d3fce043bae
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-04-10 19:17:21 -0600

    Class (libs/jobdatahandler) created, relevant functions moved out of Career.js.

--- src/Career.js
+++ src/Career.js
@@ -1,90 +1,26 @@
+import JobDataHandler from "./libs/jobdatahandler";
+
 let jobsData =
-[{
-    yearsOfExperienceAtEachStep:
-        [0, 4, 5, 8],
-    incomeAtBeginningOfEachStep:
-        [60000, 120000, 150000, 300000],
-},{
-    immediateIncome: 35000,
-    incomeCeiling: 70000,
-    yearIncomeBegins: 5,
-    yearsToIncomeCeiling: 5,
-}];
+    [{
+        yearsOfExperienceAtEachStep:
+            [0, 4, 5, 8],
+        incomeAtBeginningOfEachStep:
+            [60000, 120000, 150000, 300000],
+    },{
+        immediateIncome: 35000,
+        incomeCeiling: 70000,
+        yearIncomeBegins: 5,
+        yearsToIncomeCeiling: 5,
+    }];
 
 let graphMaxNumberOfYears = 15 - 1;
-
-
-
 var cc = console.log;
 
+
 function JobContainer({jobsData}) {
-    //let x = separateLinearAndSteppedIncomeJobs(jobsData);
+    const linearIncome = new JobDataHandler(jobsData).findLinear();
+    cc(linearIncome);
 
-/*    function separateLinearAndSteppedIncomeJobs(jobsData){
-        let steppedIncomeJobs = [], linearIncomeJobs = [];
-
-        jobsData.forEach(job => {
-            if (job.yearsOfExperienceAtEachStep){
-                steppedIncomeJobs.push(job);
-            } else if (job.immediateIncome) {
-                linearIncomeJobs.push(job);
-            }
-        });
-
-        return [steppedIncomeJobs, linearIncomeJobs];
-    }*/
-
-    let y = findLinearIncomeJobs(jobsData);
-    let z = findSteppedIncomeJobs(jobsData);
-
-    cc( y, z );
-
-    function findLinearIncomeJobs(jobsData){
-        let linearIncomeJobs = [];
-
-        jobsData.forEach(job => {
-            if (job.immediateIncome) {
-                linearIncomeJobs.push(job);
-            }
-        });
-
-        return [linearIncomeJobs];
-    }
-
-    function findSteppedIncomeJobs(jobsData){
-        let steppedIncomeJobs = [];
-
-        jobsData.forEach(job => {
-            if (job.yearsOfExperienceAtEachStep) {
-                steppedIncomeJobs.push(job);
-            }
-        });
-
-        return [steppedIncomeJobs];
-    }
-
-
-
-    function doesLinearIncomeTimeframeExceedLimit(job){
-        cc(job);
-        if ((job.yearIncomeBegins + job.yearsToIncomeCeiling) <= graphMaxNumberOfYears){
-        } else {
-            //throw error: exceeds graph's limit
-        }
-    }
-
-    function calculateLinearIncomeEachYear(){
-
-    }
-
-
-    function sumLinearIncome(job){
-
-    }
-
-    function calculateSteps(){
-
-    }
 
     return (
       <>
