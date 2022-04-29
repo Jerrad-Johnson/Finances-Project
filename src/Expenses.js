efe6de364de268bbfdc11c11065f5604d1bc128f
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-04-28 22:00:03 -0600

    Started Expenses component. Added an input field for expenses; partially finished.

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,9 +1,50 @@
+import {useState} from "react";
+
+let cc = console.log;
+function ExpensesContainer(){
+    let fieldsStateKey = 0;
+    const [expenseFieldsState, setExpenseFieldsState] = useState([fieldsStateKey]);
+
+    return (
+        <>
+            <ExpenseForms
+                expenseFieldsState = {expenseFieldsState}
+                setExpenseFieldState = {setExpenseFieldsState}
+                fieldsStateKey = {fieldsStateKey}
+            />
+        </>
+    );
+}
+
+
+function ExpenseForms({expenseFieldsState, setExpenseFieldState, fieldsStateKey}){
+    cc(expenseFieldsState)
+    let expenseForms = expenseFieldsState.map(entry => {
+        return (
+            <>
+                <input type="text" className={"inputField text-black"} key={fieldsStateKey} defaultValue={""}></input>
+            </>
+       );
+    });
+
+    return (
+        <>
+            <form>
+                {expenseForms}
+            </form>
+        </>
+    );
+}
+
 
 
 function Expenses(){
 
     return(
         <>
+          <br/>
+            <span className="inputHeader"> Type of Expense(s) </span>
+            <ExpensesContainer />
         </>
     );
 }
