665f0a9e95d7ab193c5dadd45ba38aedc05a3362
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-04-30 20:43:07 -0600

    removed useless functions in expensedatahandler.js

--- src/libs/expensedatahandler.js
+++ src/libs/expensedatahandler.js
@@ -1,6 +1,6 @@
 import Jobdatahandler from "./jobdatahandler";
 
-class ExpenseDataHandler{
+class ExpenseDataHandler {
     constructor(expenseData) {
         this.expenseData = expenseData;
         this.cc = console.log;
@@ -9,27 +9,23 @@ class ExpenseDataHandler{
         this.key = 0;
     }
 
-    beginCalculations(){
+    beginCalculations() {
         let sheet = this.expenseData;
         sheet.numberOfEntries = sheet.amount.length;
         sheet = this.convertMonthlyExpenseToYearly(sheet);
-        //sheet = this.entryYearRange(sheet);
         sheet = this.addArrayOfNumberedYearsForGraph(sheet);
         sheet = this.runningSumEachEntry(sheet);
         sheet = this.finalSumEachEntry(sheet);
         sheet = this.createRunningSumObjectForGraph(sheet);
         sheet = this.createSumObjectForGraph(sheet);
         sheet = this.createYearsForSumEachEntry(sheet);
-        sheet = this.finalSumPerExpense(sheet);
-        this.cc(sheet);
-
         return sheet;
     }
 
-    convertMonthlyExpenseToYearly(sheet){
+    convertMonthlyExpenseToYearly(sheet) {
         sheet.calculatedAmount = [];
-        for (let i = 0; i < sheet.numberOfEntries; i++){
-            if (sheet.frequency[i] == 'mo'){
+        for (let i = 0; i < sheet.numberOfEntries; i++) {
+            if (sheet.frequency[i] == 'mo') {
                 sheet.calculatedAmount[i] = sheet.amount[i] * 12;
             } else {
                 sheet.calculatedAmount[i] = sheet.amount[i];
@@ -39,39 +35,24 @@ class ExpenseDataHandler{
         return sheet;
     }
 
-    entryYearRange(sheet){
-        sheet.yearRangeForEachEntry = [];
-        let placeholder = [];
+    addArrayOfNumberedYearsForGraph(sheet) {
+        sheet.yearsNumbered = [];
 
-        for (let i = 0; i < sheet.numberOfEntries; i++){
-            for (let j = 1; j < sheet.endYears[i] +1; j++) {
-                placeholder.push(j);
-            }
-            sheet.yearRangeForEachEntry.push(placeholder);
-            placeholder = [];
+        for (let i = 0; i <= (this.graphMaxNumberOfYears - 1); i++) {
+            sheet.yearsNumbered.push("Year " + (i + 1));
         }
 
         return sheet;
     }
 
-    addArrayOfNumberedYearsForGraph(sheet){
-        sheet.yearsNumbered = [];
-
-            for (let i = 0; i <= (this.graphMaxNumberOfYears - 1); i++){
-                sheet.yearsNumbered.push("Year " + (i + 1));
-            }
-
-        return sheet;
-    }
-
-    runningSumEachEntry(sheet){
+    runningSumEachEntry(sheet) {
         sheet.runningSumsByYear = []
         let placeholder = [];
 
 
-        for (let i = 0; i < sheet.numberOfEntries; i++){
-            for (let j = 0; j < this.graphMaxNumberOfYears; j++){
-                if ((j+1) < sheet.beginYears[i]){
+        for (let i = 0; i < sheet.numberOfEntries; i++) {
+            for (let j = 0; j < this.graphMaxNumberOfYears; j++) {
+                if ((j + 1) < sheet.beginYears[i]) {
                     placeholder.push(0);
                 } else if ((j) < sheet.endYears[i]) {
                     j > 0 ? placeholder.push(sheet.calculatedAmount[i] + +placeholder[j - 1]) : placeholder.push(sheet.calculatedAmount[i]);
@@ -79,8 +60,8 @@ class ExpenseDataHandler{
                     placeholder.push(placeholder[j - 1]);
                 }
             }
-                sheet.runningSumsByYear.push(placeholder);
-                placeholder = [];
+            sheet.runningSumsByYear.push(placeholder);
+            placeholder = [];
         }
 
         return sheet;
@@ -98,15 +79,15 @@ class ExpenseDataHandler{
     }
 
 
-    createRunningSumObjectForGraph(sheet){
+    createRunningSumObjectForGraph(sheet) {
         let x = [];
         sheet.graphRunningSumObject = []
 
-        for (let i = 0; i < sheet.numberOfEntries; i++){
+        for (let i = 0; i < sheet.numberOfEntries; i++) {
             x = {};
             x.name = sheet.label[i];
             x.data = [];
-            for (let j = 0; j < this.graphMaxNumberOfYears; j++){
+            for (let j = 0; j < this.graphMaxNumberOfYears; j++) {
                 x.data.push(sheet.runningSumsByYear[i][j]);
             }
             sheet.graphRunningSumObject[i] = x;
@@ -114,10 +95,10 @@ class ExpenseDataHandler{
         return sheet;
     }
 
-    createSumObjectForGraph(sheet){
+    createSumObjectForGraph(sheet) {
         sheet.graphSumObject = []
 
-        for (let i = 0; i < sheet.numberOfEntries; i++){
+        for (let i = 0; i < sheet.numberOfEntries; i++) {
             sheet.graphSumObject[i] = {};
             sheet.graphSumObject[i].name = ""
             sheet.graphSumObject[i].name = sheet.label[i];
@@ -126,15 +107,15 @@ class ExpenseDataHandler{
         return sheet;
     }
 
-    createYearsForSumEachEntry(sheet){
+    createYearsForSumEachEntry(sheet) {
         let x = {};
 
-        for (let i = 0; i < sheet.numberOfEntries; i++){
+        for (let i = 0; i < sheet.numberOfEntries; i++) {
             x.sumByYear = []
             for (let j = 0; j < this.graphMaxNumberOfYears; j++) {
                 if ((j + 1) < sheet.beginYears[i]) {
                     x.sumByYear.push(0);
-                } else if ((j+1) > sheet.endYears[i]) {
+                } else if ((j + 1) > sheet.endYears[i]) {
                     x.sumByYear.push(0);
                 } else {
                     x.sumByYear.push(sheet.calculatedAmount[i]);
@@ -142,23 +123,6 @@ class ExpenseDataHandler{
             }
             sheet.graphSumObject[i].data = x.sumByYear;
         }
-        return sheet;
-    }
-
-    finalSumPerExpense(sheet){
-        sheet.graphSumEachExpense = {};
-        let x = {};
-        x.data = [];
-
-        for (let i = 0; i < sheet.numberOfEntries; i++){
-            x.name = sheet.label[i];
-            x.data[i] = sheet.calculatedAmount[i];
-            sheet.graphSumEachExpense[i] = x;
-            x.data = [];
-            x.name = "";
-
-        }
-
         return sheet;
     }
 }
