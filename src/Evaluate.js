9d4c0242949be960df55ee697017aff7d41e4315
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-05-13 17:58:14 -0600

    Adding functionality to Evaluation page.

--- src/Evaluate.js
+++ src/Evaluate.js
@@ -1,8 +1,67 @@
+let cc = console.log
+
+function SelectOptions({financialData}){
+    let printToDom = financialData.map((e, index) => {
+        return(
+           <option key={index}>{e.title}</option>
+        );
+    });
+    
+    return(
+        <>
+            {printToDom}
+        </>
+    );
+}
+
 function Evaluate(){
+    let linearJob = JSON.parse(localStorage.getItem("linearjob"));
+    let steppedJob = JSON.parse(localStorage.getItem("steppedjob"));
+    let investmentData = JSON.parse(localStorage.getItem("investmentdata"));
+    let expenseData = JSON.parse(localStorage.getItem("expensedata"));
+    let incomeData = [...linearJob, ...steppedJob];
+
+    cc(incomeData)
 
     return (
-        <>
-        </>
+        <div className={"container"}>
+            <br />
+            <form>
+                <select>
+                    <SelectOptions
+                        financialData = {incomeData}
+                    />
+                </select>
+                Income &nbsp;
+
+                <select>
+                    <option>2</option>
+                </select>&nbsp;
+                Expenses&nbsp;
+                <select>
+                    <option>3</option>
+                </select>&nbsp;
+                Investments&nbsp;
+                <br />
+                <br />
+                <select>
+                    <option>4</option>
+                </select>&nbsp;
+                Graph Type&nbsp;
+            </form>
+            <br />
+            <br />
+            <span>Sums</span>
+            <br />
+            <span>Income $, Expenses $, Investment Value $</span>
+            <br />
+            <span>Difference $</span>
+            <br />
+            <br />
+            <span>Graph Here</span>
+
+
+        </div>
     );
 }
 
