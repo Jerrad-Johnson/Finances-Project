a4577c07827755b034311ecc251f9d79325969e3
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-05-22 01:04:09 -0600

    Refactoring CreateNewDataForEvaluationGraphs.js

--- src/components/jobssharedfunctions.js
+++ src/components/jobssharedfunctions.js
@@ -1,5 +1,6 @@
 //TODO Add delete confirmation.
 import JobDataHandler from "../libs/jobdatahandler";
+import jobdatahandler from "../libs/jobdatahandler";
 
 var lengthOfGraphInYears = new JobDataHandler();
 lengthOfGraphInYears = lengthOfGraphInYears.graphMaxNumberOfYears;
@@ -73,4 +74,52 @@ export function createArrayOfZeros(length){
 
 export function cc(obj){
     return console.dir(obj);
+}
+
+export function isObject(x){
+    return (typeof x === 'object' && !Array.isArray(x) && x !== null)
+}
+
+export function isEmptyObject(obj) {
+    if (isObject(obj)) {
+        return Object.keys(obj).length === 0;
+    } else {
+        return true;
+    }
+}
+
+export function isEmptyArray(x) {
+    return (Array.isArray(x) && x.length === 0);
+}
+
+export function combineSinglePropertySubArrays(arr, lengthOfArray){
+    let x = [];
+
+    for (let j = 0; j < lengthOfArray; j++) {
+        x[j] = 0;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < lengthOfArray; j++) {
+            x[j] = x[j] + arr[i][j];
+        }
+    }
+
+    return x;
+}
+
+export function combineObjectKeySubArraySumsIntoNewKey(arr, oldKey, newKey, lengthOfArray){
+    arr[newKey] = [];
+
+    for (let j = 0; j < arr[oldKey].length; j++){
+        for (let i = 0; i < lengthOfArray; i++) {
+            if (isNumeric(arr[newKey][i])){
+                arr[newKey][i] = arr[newKey][i] + arr[oldKey][j][i];
+            } else {
+                arr[newKey][i] = arr[oldKey][j][i];
+            }
+        }
+    }
+
+    return arr;
 }
--- src/libs/CreateNewDataForEvaluationGraphs.js
+++ src/libs/CreateNewDataForEvaluationGraphs.js
@@ -1,5 +1,5 @@
 import jobdatahandler from "./jobdatahandler";
-import {applyRoundingSingleDepthArray, cc, createArrayOfZeros, isNumeric} from "../components/jobssharedfunctions";
+import {applyRoundingSingleDepthArray, cc, createArrayOfZeros, isNumeric, isObject, isEmptyObject, isEmptyArray} from "../components/jobssharedfunctions";
 import expenses from "../Expenses";
 import CalculateTaxes from "./Taxes";
 import investments from "../Investments";
@@ -7,10 +7,11 @@ import investments from "../Investments";
 class CreateNewDataForEvaluationGraphs {
     constructor(income, expenses, investments, employmentState = [],
                 filingStatusState = [], stTaxState = [], taxYearState = 22) {
+        this.length = new jobdatahandler().graphMaxNumberOfYears;
         this.cc = console.dir;
-        this.income = this.addValuesToIncome(income);
-        this.expenses = this.addValuesToExpenses(expenses);
-        this.investments = this.addValuesToInvestments(investments);
+        this.income = this.addValuesToIncome(income, expenses, investments, this.length);
+        this.expenses = this.addValuesToExpenses(income, expenses, investments, this.length);
+        this.investments = this.addValuesToInvestments(income, expenses, investments, this.length);
         //this.taxesOnIncomeOnly = taxes;
         this.taxesOnIncomeOnly = this.getIncomeTaxData(income, employmentState, filingStatusState, taxYearState)
         this.taxesOnIncomeAndInvestmentIncreases = this.combineIncomeAndInvestmentIncreases(this.income, this.investments, employmentState, filingStatusState, stTaxState, taxYearState);
@@ -18,7 +19,6 @@ class CreateNewDataForEvaluationGraphs {
         this.employmentState = employmentState;
         this.filingState = filingStatusState;
         this.taxYearState = taxYearState;
-        this.length = new jobdatahandler().graphMaxNumberOfYears;
         this.colors = ["#ff0000", "#00ff00", "#0000ff"]
     }
 
@@ -32,13 +32,13 @@ class CreateNewDataForEvaluationGraphs {
         this.cc(this.taxesOnIncomeOnly);*/
 
         y = this.addIncomeData(this.income, this.income?.sumByYear);
-        if (!this.isEmptyObject(y)) x.push(y); y = {};
+        if (!isEmptyObject(y)) x.push(y); y = {};
 
         y = this.addExpenseData(this.expenses, this.expenses.combinedSumByYear);
-        if (!this.isEmptyObject(y)) x.push(y); y = {};
+        if (!isEmptyObject(y)) x.push(y); y = {};
 
         y = this.addInvestmentData(this.investments, this.investments.arrayPullValueByYearPlusWithdrawl, "Investment Pulls");
-        if (!this.isEmptyObject(y)) x.push(y); y = {};
+        if (!isEmptyObject(y)) x.push(y); y = {};
 
      /*   this.cc(this.income);
         this.cc(this.expenses);
@@ -49,43 +49,43 @@ class CreateNewDataForEvaluationGraphs {
     }
 
     addIncomeData(income, incomeValueArray, sheetType = "Income"){
-        if (this.isObject(income)){
+        if (isObject(income)){
            return this.addGraphNecessities(incomeValueArray, sheetType, this.colors[0]);
         }
     }
 
     addInvestmentData(investment, investmentValueArray, sheetType = "Investment"){
-        if (this.isObject(investment)){
+        if (isObject(investment)){
             return this.addGraphNecessities(investmentValueArray, sheetType, this.colors[1]);
         }
     }
 
     addExpenseData(expense, expenseValueArray, sheetType = "Expenses"){
-        if (this.isObject(expense)){
+        if (isObject(expense)){
             return this.addGraphNecessities(expenseValueArray, sheetType, this.colors[2]);
         }
     }
 
-    addValuesToIncome(income){
-        if (this.isObject(income)) {
+    addValuesToIncome(income, expenses, investments, length){
+        if (isObject(income)) {
             income = this.makeLinearAndSteppedJobKeyNamingConsistent(income);
         }
 
         return income;
     }
 
-    addValuesToExpenses(expenses){
-        if (this.isObject(expenses)) {
-            expenses = this.combineSums(expenses, "graphSumObject", "combinedSumByYear");
+    addValuesToExpenses(income, expenses, investments, length){
+        if (isObject(expenses)) {
+            expenses = this.combineObjectArraySums(expenses, "graphSumObject", "combinedSumByYear", length);
         }
 
         return expenses;
     }
 
-    addValuesToInvestments(investments){
-        if (this.isObject(investments)) {
-            this.combineSums(investments, "arrayPullValueByYear", "arrayCombinedPullValuesByYear");
-            this.combineInvestmentsAndAdlInvestments(investments);
+    addValuesToInvestments(income, expenses, investments, length){
+        if (isObject(investments)) {
+            this.combineObjectArraySums(investments, "arrayPullValueByYear", "arrayCombinedPullValuesByYear", length);
+            this.combineInvestmentsAndAdlInvestments(income, investments);
         }
 
         return investments;
@@ -122,7 +122,8 @@ class CreateNewDataForEvaluationGraphs {
     getIncomeTaxData(income, employmentStatus, filingStatus, stTax){
         let incomeTaxData = [];
 
-        if (typeof income === 'object' && !Array.isArray(income) && income !== null){
+        //if (typeof income === 'object' && !Array.isArray(income) && income !== null){ This seems to be replaced effectively by the next line.
+        if (!isEmptyArray(income)){
             incomeTaxData = new CalculateTaxes(income.salaryAmounts || income.incomeInGraphYearsNumberOfSteps, employmentStatus,
                 filingStatus, stTax, "22");
             incomeTaxData = incomeTaxData.federalCalculations();
@@ -143,29 +144,33 @@ class CreateNewDataForEvaluationGraphs {
     }
 
     combineIncomeAndInvestmentIncreases(income, investments, employmentState, filingStatusState, stTaxState, taxYearState){
-        let length = new jobdatahandler().graphMaxNumberOfYears;
-        let investmentProfits = this.combineSinglePropertyArrays(this.investments.arrayInvestmentIncreaseByYearMinusAllYearAdlInvestment);
-        let incomeTogether = [];
-        incomeTogether.push(income.sumByYear);
-        incomeTogether.push(investmentProfits);
-        let incomeCombined = [];
+        if (!isEmptyArray(income) && !isEmptyArray(investments)) {
+            let length = new jobdatahandler().graphMaxNumberOfYears;
+            let investmentProfits = this.combineSinglePropertyArrays(this.investments.arrayInvestmentIncreaseByYearMinusAllYearAdlInvestment);
+            let incomeTogether = [];
+            incomeTogether.push(income.sumByYear);
+            incomeTogether.push(investmentProfits);
+            let incomeCombined = [];
 
-        for (let j = 0; j < incomeTogether.length; j++){
-            for (let i = 0; i < length; i++){
-                if (isNumeric(incomeCombined[i])) {
-                    incomeCombined[i] = incomeCombined[i] + incomeTogether[j][i];
-                } else {
-                    incomeCombined[i] = incomeTogether[j][i];
+            for (let j = 0; j < incomeTogether.length; j++) {
+                for (let i = 0; i < length; i++) {
+                    if (isNumeric(incomeCombined[i])) {
+                        incomeCombined[i] = incomeCombined[i] + incomeTogether[j][i];
+                    } else {
+                        incomeCombined[i] = incomeTogether[j][i];
+                    }
                 }
             }
+
+            let x = new CalculateTaxes(incomeCombined, employmentState, filingStatusState, stTaxState, taxYearState).federalCalculations();
+
+            return x;
         }
-
-        let x = new CalculateTaxes(incomeCombined, employmentState, filingStatusState, stTaxState, taxYearState).federalCalculations();
-
-        return x;
     }
 
-    combineInvestmentsAndAdlInvestments(investments){
+    combineInvestmentsAndAdlInvestments(income, investments){
+        if (!isEmptyArray(income) && !this.isEmptyArray(investments)) {
+
         let moneyInvested = this.combineSinglePropertyArrays(investments.arrayAdditionalInvestment);
 
         for (let i = 0; i < investments.amounts.length; i++){
@@ -173,11 +178,11 @@ class CreateNewDataForEvaluationGraphs {
         }
 
         return moneyInvested;
+        }
     }
 
 
-    combineSums(arrs, oldKey, newKey){
-        let length = new jobdatahandler().graphMaxNumberOfYears;
+    combineObjectArraySums(arrs, oldKey, newKey, length){
         arrs[newKey] = [];
 
         for (let j = 0; j < arrs[oldKey].length; j++){
@@ -197,24 +202,6 @@ class CreateNewDataForEvaluationGraphs {
         return arrs;
     }
 
-    combineSinglePropertyArrays(financialDataProperty){
-        let length = new jobdatahandler().graphMaxNumberOfYears;
-        let x = [];
-
-
-        for (let j = 0; j < length; j++) {
-            x[j] = 0;
-        }
-
-        for (let i = 0; i < financialDataProperty.length; i++) {
-            for (let j = 0; j < length; j++) {
-                x[j] = x[j] + financialDataProperty[i][j];
-            }
-        }
-
-        return x;
-    }
-
     combineMultiplePropertyArrays(financialDataProperties){
         let x = [];
 
@@ -231,21 +218,7 @@ class CreateNewDataForEvaluationGraphs {
         return x;
     }
 
-    isObject(x){
-        return (typeof x === 'object' && !Array.isArray(x) && x !== null)
-    }
 
-    isEmptyObject(obj) {
-        if (this.isObject(obj)) {
-            return Object.keys(obj).length === 0;
-        } else {
-            return true;
-        }
-    }
-
-    checkIfEmptyArray(x) {
-        return (Array.isArray(x) && x.length === 0);
-    }
 }
 
 export default CreateNewDataForEvaluationGraphs;
