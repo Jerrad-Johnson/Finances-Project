bfb07fb1c31a157385cb48ac474898a1239a88fa
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-06-01 20:44:04 -0600

    Changed background color

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ function Home(){
     return (
       <div className={"homeContainer"}>
           <h1>Welcome</h1>
-          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+          <span>This project is still under construction. Please excuse any bugs, poor mobile support, etc.</span>
+          <span></span>
       </div>
     );
 }
