8079740faada763f3882467ec5d3a3da459f72ac
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-06-14 17:46:56 -0600

    Refactored Cypress graph test file.

--- cypress/integration/EvaluationGraphs.js
+++ cypress/integration/EvaluationGraphs.js
@@ -4,7 +4,6 @@ Cypress.Commands.add("getSampleData", () => {
 });
 
 Cypress.Commands.add("tryAllGraphTypes", () => {
-
     let graphOptions = ["Running Liquid Assets Sums vs. Yearly Expenses", "Total Assets", "Assets vs. Inflation",
         "Combined Investment Expenses", "Expenses by Group", "Income by Group", "Investment Income",
         "Same Year Expendable", "Taxes by Category", "Tax Percentage by Year"];
@@ -23,6 +22,7 @@ Cypress.Commands.add("tryAllDatasetOptions", (graphOneClassName, graphTwoClassNa
         cy.getSampleData();
         let dataChoice;
         i == 0 ? dataChoice = 0 : dataChoice = "No Data";
+
         cy.get(graphOneClassName).select(graphOneName);
         cy.get(graphTwoClassName).select(graphTwoName);
         cy.get(".incomeDataset").select(dataChoice);
@@ -34,9 +34,10 @@ Cypress.Commands.add("tryAllDatasetOptions", (graphOneClassName, graphTwoClassNa
         for (let j = 0; j < 2; j++){
             cy.getSampleData();
             let dataChoice;
+            j == 0 ? dataChoice = 0 : dataChoice = "No Data";
+
             cy.get(graphOneClassName).select(graphOneName);
             cy.get(graphTwoClassName).select(graphTwoName);
-            j == 0 ? dataChoice = 0 : dataChoice = "No Data";
             cy.get(".incomeDataset").select(dataChoice);
             cy.get(".expensesDataset").select(dataChoice);
             cy.get(".investmentDataset").select(dataChoice);
@@ -46,9 +47,10 @@ Cypress.Commands.add("tryAllDatasetOptions", (graphOneClassName, graphTwoClassNa
             for (let k = 0; k < 2; k++){
                 cy.getSampleData();
                 let dataChoice;
+                k == 0 ? dataChoice = 0 : dataChoice = "No Data";
+
                 cy.get(graphOneClassName).select(graphOneName);
                 cy.get(graphTwoClassName).select(graphTwoName);
-                k == 0 ? dataChoice = 0 : dataChoice = "No Data";
                 cy.get(".incomeDataset").select(dataChoice);
                 cy.get(".expensesDataset").select(dataChoice);
                 cy.get(".investmentDataset").select(dataChoice);
@@ -57,71 +59,6 @@ Cypress.Commands.add("tryAllDatasetOptions", (graphOneClassName, graphTwoClassNa
             }
         }
     }
-
-/*
-    cy.getSampleData();
-    cy.get(".incomeDataset").select("No Data");
-    cy.get(".expensesDataset").select(0);
-    cy.get(".investmentDataset").select(0);
-    cy.get(".apexcharts-canvas").eq(0).should("exist");
-    cy.get(".apexcharts-canvas").eq(1).should("exist");
-    //011
-
-    cy.getSampleData();
-    cy.get(".incomeDataset").select("No Data");
-    cy.get(".expensesDataset").select("No Data");
-    cy.get(".investmentDataset").select(0);
-    cy.get(".apexcharts-canvas").eq(0).should("exist");
-    cy.get(".apexcharts-canvas").eq(1).should("exist");
-    //001
-
-    cy.getSampleData();
-    cy.get(".incomeDataset").select("No Data");
-    cy.get(".expensesDataset").select("No Data");
-    cy.get(".investmentDataset").select("No Data");
-    cy.get(".apexcharts-canvas").eq(0).should("exist");
-    cy.get(".apexcharts-canvas").eq(1).should("exist");
-    //000
-
-    cy.getSampleData();
-    cy.get(".incomeDataset").select("No Data");
-    cy.get(".expensesDataset").select(0);
-    cy.get(".investmentDataset").select(0);
-    cy.get(".apexcharts-canvas").eq(0).should("exist");
-    cy.get(".apexcharts-canvas").eq(1).should("exist");
-    //011
-
-    cy.getSampleData();
-    cy.get(".incomeDataset").select("No Data");
-    cy.get(".expensesDataset").select(0);
-    cy.get(".investmentDataset").select("No Data");
-    cy.get(".apexcharts-canvas").eq(0).should("exist");
-    cy.get(".apexcharts-canvas").eq(1).should("exist");
-    //010
-
-    cy.getSampleData();
-    cy.get(".incomeDataset").select(0);
-    cy.get(".expensesDataset").select("No Data");
-    cy.get(".investmentDataset").select(0);
-    cy.get(".apexcharts-canvas").eq(0).should("exist");
-    cy.get(".apexcharts-canvas").eq(1).should("exist")
-    //101
-
-    cy.getSampleData();
-    cy.get(".incomeDataset").select(0);
-    cy.get(".expensesDataset").select("No Data");
-    cy.get(".investmentDataset").select("No Data");
-    cy.get(".apexcharts-canvas").eq(0).should("exist");
-    cy.get(".apexcharts-canvas").eq(1).should("exist")
-    //100
-
-    cy.getSampleData();
-    cy.get(".incomeDataset").select(0);
-    cy.get(".expensesDataset").select(0);
-    cy.get(".investmentDataset").select("No Data");
-    cy.get(".apexcharts-canvas").eq(0).should("exist");
-    cy.get(".apexcharts-canvas").eq(1).should("exist")
-    //110*/
 });
 
 describe("Evaluate page checks", () => {
