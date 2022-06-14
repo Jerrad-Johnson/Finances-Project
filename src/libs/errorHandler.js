c734db2cae27d266073f9d8f1700d8cb6e18ad85
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-06-13 23:15:05 -0600

    Working on errorHandler.js

--- src/libs/errorHandler.js
+++ src/libs/errorHandler.js
@@ -1,5 +1,31 @@
 export function errorHandler(message){
-    alert(message);
+    //alert(message);
+
+    let buttonFunction = (() => {
+       console.log(5);
+    });
+
+    let x = document.createElement("div");
+    x.setAttribute("class", "errorMessage");
+    x.innerText = message;
+
+    let y = document.createElement("button");
+    y.innerText = "Close"
+    y.setAttribute("onClick", buttonFunction);
+
+    x.append(y);
+
+    let z = document.querySelector("body");
+
+    z.append(x);
+
+
+
+    //let y = (<div><button>${message}</button></div>);
+    //console.log(y);
+
+    console.log(x)
+
 }
 
 
