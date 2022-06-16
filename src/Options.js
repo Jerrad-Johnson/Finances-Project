e1c8ddb41323cbea3fa8cfde168121718c5c0b07
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-06-16 11:57:58 -0600

    Styled Options page and redesigned Max Length selector; removed "Current" value.

--- src/Options.js
+++ src/Options.js
@@ -1,28 +1,47 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import {cc} from "./components/jobssharedfunctions";
+import EvaluationForms from "./components/EvaluationForms";
+import EvaluationGraphs from "./components/EvaluationGraphs";
+import EvaluationGeneralData from "./components/EvaluationGeneralData";
 
 function Options(){
     //let [graphMaxLengthState, setGraphMaxLengthState] = useState()
                 //localStorage.removeItem("graphMaxLength");
 
     return (
-      <>
-        <form>
-            <select defaultValue="Current" onChange={((e) => {
-               e.preventDefault();
-               if (e.target.value !== "Current") {
-                   let optionSelected = e.target.value;
-                   optionSelected = +optionSelected
-                   localStorage.setItem("graphMaxLength", JSON.stringify(optionSelected));
-               }
-            })}>
-                <option>Current</option>
-                <option>15</option>
-                <option>20</option>
-                <option>25</option>
-            </select>
-        </form>
-      </>
+        <div className={"container"}>
+            <div className={"pairs"}>
+                <div className={"left"}>
+                    <div className={"inputSet"}>
+                        <div className={"inputSelectorsCard"}>
+                            <span className={"inputSetTitle"}>Options</span>
+                            <form>
+                                <span className={"inputTitle"}>Max Length in Years</span>
+                                <select className={"inputSelector"} defaultValue={JSON.parse(localStorage.getItem("graphMaxLength")) || 15} onChange={((e) => {
+                                    e.preventDefault();
+                                        let optionSelected = e.target.value;
+                                        optionSelected = +optionSelected
+                                        localStorage.setItem("graphMaxLength", JSON.stringify(optionSelected));
+                                })}>
+                                    <option>15</option>
+                                    <option>20</option>
+                                    <option>25</option>
+                                </select>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"right"}>
+                    <div className={"graphCard"}>
+                    Lorem Ipsum
+                    </div>
+                </div>
+            </div>
+            <EvaluationGeneralData />
+        </div>
+
+
     );
 }
 
